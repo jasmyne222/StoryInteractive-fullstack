2de85b97b26785c9a7f7853a2101987301b19a43
Chabloz Nicolas (SEC-GC) <CHABLOZN@ge-admin.ad.etat-ge.ch>
2025-04-29 08:26:01 +0200

    Add comment

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ Route::get('/', function () {
   return view('test');
 });
 
-// Instead of using Sanctum (or something similar) for authentication,
+// Instead of using Sanctum (or something similar) for API authentication,
 // we are using the built-in Laravel session authentication system.
 require_once __DIR__ . '/api.php';
